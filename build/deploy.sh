1677a61c0f34575c5d086a860a899bc83ff4460f
ia <isaac.ardis@gmail.com>
2019-01-06 13:38:26 -0600

    fix? travis release permissions fail, add one sha/execs

--- build/deploy.sh
+++ build/deploy.sh
@@ -12,3 +12,13 @@ zip -j "$ALLTOOLS_ARCHIVE_NAME.zip" build/bin/*
 shasum -a 256 $ALLTOOLS_ARCHIVE_NAME.zip
 shasum -a 256 $ALLTOOLS_ARCHIVE_NAME.zip > $ALLTOOLS_ARCHIVE_NAME.zip.sha256
 
+ALLTOOLS_SHAS_FILE="$ALLTOOLS_ARCHIVE_NAME-sha256s.txt"
+for f in build/bin/*; do
+	shasum -a 256 "$f"
+done | while read -r sum p; do
+	line="$sum ${p##*/}"
+	echo "$line" | tee -a "$ALLTOOLS_SHAS_FILE"
+done
+
+export ALLTOOLS_SHAS_SHA=$(shasum -a 256 "$ALLTOOLS_SHAS_FILE") # one sha to bind them...
+echo "$ALLTOOLS_SHAS_SHA" | tee "$ALLTOOLS_SHAS_SHA.sha256"
